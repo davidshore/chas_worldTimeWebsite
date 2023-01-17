02afddf14091f531a249e6b844632e402aa0413d
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-17 12:24:13 +0100

    Create home.jsx

---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+            <Clock city="Paris" timeZone="Europe/Paris" />
+          <Clock city="Stockholm" timeZone="Europe/Stockholm" />
+    </div>
+  )
+}
