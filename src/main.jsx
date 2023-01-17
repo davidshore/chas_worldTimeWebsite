be4a2b63202140059574337e631446c696b5ddb8
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-17 12:24:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,31 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "clock/:cityKey",
+        element: <SingleClock />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
