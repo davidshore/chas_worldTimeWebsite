7a1160dad32330b377ae0d6a2f14343c4392a276
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-21 20:03:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import Clock from "./Clock";
+import { Route, Routes } from "react-router-dom"
+import WorldTime from "./WorldTime";
+import Navigation from "./Navigation";
+import Stockholm from "./components/Stockholm";
+import London from "./components/London";
+import NewYork from "./components/NewYork";
 
 function App() {
   return (
+    
     <div>
-      <nav className="flex justify-between p-8 bg-blue-400 items-center">
-        <h1 className="text-4xl font-extrabold text-white">World Time</h1>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={ <WorldTime/> } />
+        <Route path="/Stockholm" element={ <Stockholm/> } />
+        <Route path="/NewYork" element={ <NewYork/> } />
+        <Route path="/London" element={ <London/> } />
 
-        <ul className="flex space-x-4">
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <main className="p-8">
-        <div className="flex flex-col md:flex-row items-center space-x-0 md:space-x-4">
-          <Clock city="Paris" timeZone="Europe/Paris" />
-          <Clock city="Stockholm" timeZone="Europe/Stockholm" />
-        </div>
-      </main>
+      </Routes>
     </div>
   );
 }
