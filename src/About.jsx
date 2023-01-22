05943911ee66778e2b0a8ff46ed293f92eb88686
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-22 11:40:51 +0100

    Create About.jsx

---
+++ src/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='text-center w-56 m-auto mt-10 min-h-[68vh]'>
+      <h1 className='bg-cyan-900 text-white text-4xl py-3 font-bold'>About</h1>
+    </div>
+  )
+}
