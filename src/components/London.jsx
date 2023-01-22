67981341bc35ebe7f272afa92287227f0916fa5d
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-22 11:41:03 +0100

    Update London.jsx

--- src/components/London.jsx
+++ src/components/London.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Clock from '../Clock'
 export default function London() {
   return (
-    <div>
-      London
+    <div className='px-10 min-h-[68vh]'>
+      <div className='text-center w-56 m-auto mt-10'>
+      <h1 className='bg-cyan-900 text-white text-4xl py-3 font-bold'>London</h1>
+      </div>
+      <div className='pt-10'>
+        <Clock city="London" timeZone="Europe/London" />
+      </div>
     </div>
   )
 }
