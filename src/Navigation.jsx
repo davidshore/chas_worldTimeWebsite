15a484c4f33a82fe1bdb3ff5a9be2efb88532599
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-22 11:41:25 +0100

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import Stockholm from "./components/Stockholm";
-import London from "./components/London"
+
 import { Link } from 'react-router-dom'
 export default function Navigation() {
   return (
-    <div>
-      <nav className="flex justify-between p-8 bg-blue-400 items-center">
-        <Link to="/"><h1 className="text-4xl font-extrabold text-white">World Time</h1></Link>
+    <div className='top-0 sticky'>
+      <nav className="flex justify-between p-8 bg-cyan-900 shadow-xl  items-center ">
+        <Link to="/"><h1 className="text-4xl font-extrabold bg-white px-6 py-5 text-black rounded-2xl">World Time</h1></Link>
 
-        <ul className="flex space-x-4">
-          <Link to="/Stockholm"><li><Stockholm/></li></Link>
-          <Link to="/Stockholm"><li><Stockholm/></li></Link>
-          <Link to="/London"><li><London/></li></Link>
-          <Link to="/Stockholm"><li><Stockholm/></li></Link>
-          <Link to="/Stockholm"><li><Stockholm/></li></Link>
+        <ul className="flex flex-col sm:flex-row ">
+            <Link to="/About"><li className='w-28  font-extrabold bg-white px-6 py-2 m-2 text-center text-black rounded-2xl' >About</li></Link> 
+            <Link to="/Contact"><li className='w-28  font-extrabold bg-white px-6 py-2 m-2 text-center text-black rounded-2xl' >Contact</li></Link> 
         </ul>
       </nav>
     </div>
