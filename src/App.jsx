c16f29ce47c7f0ca214280a2072a995f6a4a2743
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-22 11:40:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import WorldTime from "./WorldTime";
 import Navigation from "./Navigation";
 import Stockholm from "./components/Stockholm";
-import London from "./components/London";
 import NewYork from "./components/NewYork";
+import Istanbul from "./components/Istanbul"
+import London from "./components/London";
+import Tokyo from "./components/Tokyo";
+import Shanghai from "./components/Shanghai";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
 
 function App() {
   return (
@@ -14,9 +21,14 @@ function App() {
         <Route path="/" element={ <WorldTime/> } />
         <Route path="/Stockholm" element={ <Stockholm/> } />
         <Route path="/NewYork" element={ <NewYork/> } />
+        <Route path="/Istanbul" element={ <Istanbul/> } />
         <Route path="/London" element={ <London/> } />
-
+        <Route path="/Tokyo" element={ <Tokyo/> } />
+        <Route path="/Shanghai" element={ <Shanghai/> } />
+        <Route path="/About" element={ <About/> } />
+        <Route path="/Contact" element={ <Contact/> } />
       </Routes>
+      <Footer/>
     </div>
   );
 }
