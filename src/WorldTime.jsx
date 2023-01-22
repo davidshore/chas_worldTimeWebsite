b818fa00d791fc5df97b08019fe6451b44e29799
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-22 11:41:29 +0100

    Update WorldTime.jsx

--- src/WorldTime.jsx
+++ src/WorldTime.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Clock from "./Clock";
+import { Link } from 'react-router-dom'
 export default function WorldTime() {
   return (
     <div>
-        <main className="p-8">
-        <div className="flex flex-col md:flex-row items-center space-x-0 md:space-x-4">
+      
+        <main className="p-8 flex justify-center items-center min-h-[73vh] ">
+        <div className="flex flex-col  md:flex-row items-center space-x-0 md:space-x-4">
           <div>
           <Clock city="Stockholm" timeZone="Europe/Stockholm" />
           <Clock city="London" timeZone="Europe/London" />
           </div>
           <div>
-          <Clock city="New York" timeZone="America/New_York" />
+          <Clock city="NewYork" timeZone="America/New_York" />
           <Clock city="Tokyo" timeZone="Asia/Tokyo" />
           </div>
           <div>
