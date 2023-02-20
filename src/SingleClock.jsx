cdf3ed838f050aa2bfeee3bd110475eb029ff1d2
ChasAcademy-Fernando <115954598+ChasAcademy-Fernando@users.noreply.github.com>
2023-02-20 19:42:54 +0100

    Done Router

--- src/SingleClock.jsx
+++ src/SingleClock.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import {timeZones} from "./timeZones"
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { timeZones } from "./timeZones";
 
-export default function Clock({ cityKey }) {
+export default function SingleClock() {
 
-  const {city, timeZone} = timeZones[cityKey]
+    const {cityKey} = useParams();
+    const {city, timeZone, country} = timeZones[cityKey]
   const [time, setTime] = useState();
 
   useEffect(() => {
@@ -19,11 +20,11 @@ export default function Clock({ cityKey }) {
       className="flex md:flex-col justify-between items-center 
     md:items-start p-6 bg-slate-200 rounded-md w-full md:w-auto mb-4"
     >
-      <div className="text-lg text-gray-600">{city}</div>
-      <div className="text-3xl text-gray-900 font-semibold font-mono">
+      <div className="text-2xl text-gray-600">{city}</div>
+      <div className="text-2xl text-blue-600">{country}</div>
+      <div className="text-4xl text-gray-900 font-semibold font-mono">
         {time}
       </div>
-      <div className=" text-gray-600 underline hover:text-gray-400"><Link to={`/clock/${cityKey}`}>Bigger</Link></div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Contact from "./Contact";
 import "./index.css";
+import About from "./About";
+import Root from "./Root";
+import SingleClock from "./SingleClock";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "clock/:cityKey",
+        element: <SingleClock />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
